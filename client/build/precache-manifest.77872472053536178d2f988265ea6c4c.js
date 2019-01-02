1d633e222eedce5e64e01c2ed57c0292b41b08de
Aly <38168385+alycapps@users.noreply.github.com>
2019-01-02 11:26:36 -0500

    no precache

--- client/build/precache-manifest.77872472053536178d2f988265ea6c4c.js
+++ client/build/precache-manifest.77872472053536178d2f988265ea6c4c.js
@@ -1,22 +1,22 @@
-self.__precacheManifest = [
-  {
-    "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
-  },
-  {
-    "revision": "486a67a710716fd9dcd9",
-    "url": "/static/js/main.486a67a7.chunk.js"
-  },
-  {
-    "revision": "512459f43277fd6acbfd",
-    "url": "/static/js/1.512459f4.chunk.js"
-  },
-  {
-    "revision": "486a67a710716fd9dcd9",
-    "url": "/static/css/main.4116cf5a.chunk.css"
-  },
-  {
-    "revision": "e51d1a6cc3442e400b7bdac60d7c9b2f",
-    "url": "/index.html"
-  }
-];
+// self.__precacheManifest = [
+//   {
+//     "revision": "229c360febb4351a89df",
+//     "url": "/static/js/runtime~main.229c360f.js"
+//   },
+//   {
+//     "revision": "486a67a710716fd9dcd9",
+//     "url": "/static/js/main.486a67a7.chunk.js"
+//   },
+//   {
+//     "revision": "512459f43277fd6acbfd",
+//     "url": "/static/js/1.512459f4.chunk.js"
+//   },
+//   {
+//     "revision": "486a67a710716fd9dcd9",
+//     "url": "/static/css/main.4116cf5a.chunk.css"
+//   },
+//   {
+//     "revision": "e51d1a6cc3442e400b7bdac60d7c9b2f",
+//     "url": "/index.html"
+//   }
+// ];
