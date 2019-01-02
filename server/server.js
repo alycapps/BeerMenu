5d9fc0b8015b8a37bfd555cbd211b7cfbf7dcc54
Aly <38168385+alycapps@users.noreply.github.com>
2019-01-02 16:36:20 -0500

    fixing routes

--- server/server.js
+++ server/server.js
@@ -31,12 +31,12 @@ if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV');
 	app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/', 'index.html'))
+		res.sendFile(path.join(__dirname, '../client/build/'))
 	});
 }
 
 // connect to path files
-app.use("/", routes);
+app.use(routes);
 
 // Error handler
 app.use(function(err, req, res, next) {
